b06e67eb1d1c557f6f159221e5a55d5be08f714e
gezichenshan YU <xtyujd@gmail.com>
2024-05-09 12:08:25 +0800

    add lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  srcDir: "src",
-  modules: ["@unocss/nuxt", "@pinia/nuxt"],
-});
+  srcDir: 'src',
+  modules: ['@unocss/nuxt', '@pinia/nuxt'],
+})
--- src/components/UI/Calender.vue
+++ src/components/UI/Calender.vue
@@ -1,7 +1,8 @@
+<script setup lang="ts">
+</script>
+
 <template>
   <div>Canlender</div>
 </template>
-
-<script setup lang="ts"></script>
 
 <style scoped></style>
