8b62762b7897a38c9bfdc143f203bd7e4aa0c254
Jiadong Yu <jiadong@pandu.ie>
2024-05-13 12:19:32 +0800

    add time button animation

--- src/components/UI/Calender/TimePicker/TimeButton/index.vue
+++ src/components/UI/Calender/TimePicker/TimeButton/index.vue
@@ -1,18 +1,41 @@
 <script setup lang="ts">
+interface Props {
+  data: { id: number, selected: boolean, time: string }
+}
+const props = defineProps<Props>()
 
+const emits = defineEmits(['select'])
+const { data } = toRefs(props)
+function handleSelect() {
+  emits('select', data.value)
+}
 </script>
 
 <template>
-  <button class="time-btn btn cursor-pointer">
-    <div class="_nsuUAEh3mYW8eVzjz1b vQ3fZCbPbugeCo9yvAwg EpkalPYkm2S0JNrW12se">
-      <div class="j1sPElHG5fkgLNJPo_Q_">
-        19:00
+  <div class="time-btn-wrap " @click="handleSelect">
+    <button class="time-btn btn text cursor-pointer" :class="[data.selected && 'selected']">
+      <div class="btn-innner ">
+        <div class="j1sPElHG5fkgLNJPo_Q_">
+          {{ data.time }}
+        </div>
       </div>
-    </div>
-  </button>
+    </button>
+    <button v-if="data.selected" class="time-btn btn next cursor-pointer" :class="[data.selected && 'selected']">
+      <div class="btn-innner ">
+        <div class="j1sPElHG5fkgLNJPo_Q_">
+          下一步
+        </div>
+      </div>
+    </button>
+  </div>
 </template>
 
 <style scoped>
+.time-btn-wrap {
+  margin: 10px;
+  font-size: 16px;
+  white-space: pre;
+}
 .time-btn {
   width: 100%;
   height: 52px;
@@ -24,6 +47,9 @@
   border-width: 2px;
   border-color: var(--primary-color, rgb(0, 105, 255));
 }
+.time-btn.selected {
+  width: 48.5%;
+}
 .btn {
   position: relative;
   vertical-align: top;
@@ -32,7 +58,7 @@
   font-weight: 700;
   transition: all 0.3s ease;
   transition-property: width, transform;
-  background-color: #fff;
+  background-color: transparent;
 }
 .btn::after {
   content: '';
@@ -43,14 +69,33 @@
   width: 100%;
 }
 
-._nsuUAEh3mYW8eVzjz1b.EpkalPYkm2S0JNrW12se {
+.btn-innner.EpkalPYkm2S0JNrW12se {
   width: 45px;
 }
-._nsuUAEh3mYW8eVzjz1b {
+.btn-innner {
   position: relative;
   margin-right: auto;
   margin-left: auto;
 }
+.text.selected {
+  border-color: transparent;
+  background-color: rgb(0 0 0 / 60%);
+  color: var(--text-color-secondary-4, rgba(255, 255, 255, 1));
+}
+.next {
+  margin-left: 3%;
+  background-color: var(--primary-color, rgb(0, 105, 255));
+  color: var(--primary-text-color, #ffffff);
+  box-shadow: 0 1px 6px 0 rgb(0 0 0/10%);
+  transform: translateX(40px);
+}
+.next.selected {
+  transform: translateX(0);
+}
+.next:hover {
+  background-color: var(--text-color-secondary-3, rgba(0, 102, 230, 1));
+}
+
 .j1sPElHG5fkgLNJPo_Q_ {
   line-height: 10px;
 }
--- src/components/UI/Calender/TimePicker/index.vue
+++ src/components/UI/Calender/TimePicker/index.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-
+const times = ref([{ id: 1, selected: false, time: '19:00' }, { id: 2, selected: false, time: '20:00' }])
+function handleSelect(item) {
+  console.log(1111, item)
+  times.value = times.value.map(t => ({ ...t, selected: item.id === t.id }))
+}
 </script>
 
 <template>
   <div style="width: 300px;">
-    <UICalenderTimePickerTimeButton />
+    <UICalenderTimePickerTimeButton v-for="(item, i) in times" :key="i" :data="item" @select="handleSelect" />
   </div>
 </template>
 
